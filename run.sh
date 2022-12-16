9f327f4e515a9e5005e6465d67c66e4b456565d1
Greg Abram <gda@tacc.utexas.edu>
2022-12-16 11:01:00 -0600

    Change the docker image name

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
-docker run -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=host.docker.internal:0 -v  `pwd`:/home/ospray -di gregabram/mympi
+docker run -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=host.docker.internal:0 -v  `pwd`:/home/ospray -di gregabram/ospray-dev
+
 
